4b3d96c10df8abd9029be172cd47d4f3282b256a
MahenoorAhsan <mahenoor.ahsan@aot.edu.in>
2024-07-13 21:58:23 +0530

    header created

--- ghc_website/src/app/app.component.ts
+++ ghc_website/src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, DatePipe } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ghc_website';
+  currentTime  = this.datePipe.transform(new Date(), 'HH:mm');
+  constructor(private datePipe: DatePipe) {}
+
+  ngOnInit() {
+    this.updateCurrentTime();
+    console.log(this.currentTime);
+  }
+  
+  updateCurrentTime() {
+    let time = this.datePipe.transform(new Date(), 'HH:mm');
+    this.currentTime=time;
+    setInterval(() => this.currentTime = this.datePipe.transform(new Date(), 'HH:mm:ss'), 60000);
+  }
 }
